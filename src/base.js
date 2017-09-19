d358d59c659b486ee4fdb72cc62fec0295546769
Sergey Smirnov <sssmirnov@hotmail.com>
2017-09-19 11:50:19 +0700

    complete react course project

---
+++ src/base.js
@@ -0,0 +1,9 @@
+import Rebase from 're-base';
+
+const base = Rebase.createClass({
+    apiKey: "AIzaSyArzKf_Q4S5u8pvcsMjVNBH0thE5tQulWM",
+    authDomain: "catch-of-the-day-aee82.firebaseapp.com",
+    databaseURL: "https://catch-of-the-day-aee82.firebaseio.com",
+});
+
+export default base;
